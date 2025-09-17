7208091fe1d9470f8acd98d4a17898403eb6ffcd
Le Dinh Thai <ledinhthai0222@gmail.com>
2025-09-17 20:09:23 +0700

    init project

--- Program.cs
+++ Program.cs
@@ -6,16 +6,16 @@ namespace _23WebC_Nhom10
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+           
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                
                 app.UseHsts();
             }
 
