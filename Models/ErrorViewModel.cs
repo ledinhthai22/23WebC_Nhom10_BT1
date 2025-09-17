55ab8ca1a4278f0c757f3fc1b03d24b883fba64a
Le Dinh Thai <ledinhthai0222@gmail.com>
2025-09-17 20:06:45 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _23WebC_Nhom10.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
